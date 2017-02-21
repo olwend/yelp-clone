dcb3a9761f4dd2c035379df1fae940dcc82d3436
olwend <olwend@gmail.com>
2017-02-21 12:56:13 +0000

    10 updating description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220150046) do
+ActiveRecord::Schema.define(version: 20170221113615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,9 @@ ActiveRecord::Schema.define(version: 20170220150046) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "student", force: :cascade do |t|
-    t.text "name", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
