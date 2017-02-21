7c482cb486d92e7fdb35649210c7c9585212853c
olwend <olwend@gmail.com>
2017-02-21 14:33:54 +0000

    11 -Destroy

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -48,17 +48,31 @@ end
 
   context 'editing restaurants' do
 
-  before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
-  scenario 'let a user edit a restaurant' do
-    visit '/restaurants'
-    click_link 'Edit KFC'
-    fill_in 'Name', with: 'Kentucky Fried Chicken'
-    fill_in 'Description', with: 'Deep fried goodness'
-    click_button 'Update Restaurant'
-    click_link 'Kentucky Fried Chicken'
-    expect(page).to have_content 'Kentucky Fried Chicken'
-    expect(page).to have_content 'Deep fried goodness'
-    expect(current_path).to eq '/restaurants/1'
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      click_link 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants/1'
+      end
     end
+  context 'deleting restaurants' do
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+
   end
+
+
 end
