245415582db6c5244436b686deda868ab4d54625
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-02-23 12:05:43 +0000

    V2_S4 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,36 @@ class User < ApplicationRecord
     end
   end
 
-  def facebook
-  #@user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+   def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+           # If you are using confirmable and the provider(s) you use validate emails,
+           # uncomment the line below to skip the confirmation emails.
+           # user.skip_confirmation!
     end
    end
 
+  #  def create
+  #   user = User.from_omniauth(env["omniauth.auth"]) #Highligted line as red
+  #   session[:user_id] = user.id
+  #   redirect_to root_url
+  # end
+
+  # def facebook
+  # #@user = User.from_omniauth(request.env["omniauth.auth"])
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  #   else
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  #  end
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
+   devise :omniauthable, :omniauth_providers => [:facebook]
 end
